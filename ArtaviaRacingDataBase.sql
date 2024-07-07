00b92e47ce5be85e1540c4536da2960b8f8cffe8
Andres <jalvarado60606@ufide.ac.cr>
2024-07-07 15:14:07 -0700

    [AR-001] Last changes in Data Bases Worked by Andres

--- ArtaviaRacingDataBase.sql
+++ ArtaviaRacingDataBase.sql
@@ -1,14 +1,155 @@
+/****************************************************************************************************************************************************************
+Autor: José Andrés Alvarado Matamoros
+Requerimiento: AR-001
+Fecha Creación: 06/07/2024   (MM/dd/YYYY)
+Enunciado: Esta tabla se creo  para guardar la informacion del puesto de trabajo
+****************************************************************************************************************************************************************/
 CREATE TABLE "HR"."PUESTOTRABAJO" 
-   (	"PUESTOTRABAJOID" NUMBER GENERATED ALWAYS AS IDENTITY MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  NOT NULL ENABLE, 
-	"PUESTO" VARCHAR2(50 BYTE) NOT NULL ENABLE, 
-	"DESCRIPCION" VARCHAR2(250 BYTE) NOT NULL ENABLE, 
-	"EDITADOPOR" VARCHAR2(10 BYTE) NOT NULL ENABLE, 
-	"HABILITADO" NUMBER(1,0) NOT NULL ENABLE, 
-	"FECHACREACION" DATE NOT NULL ENABLE, 
-	 PRIMARY KEY ("PUESTOTRABAJOID")
+(	
+     "PUESTOTRABAJOID" NUMBER GENERATED ALWAYS AS IDENTITY MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  NOT NULL ENABLE 
+    ,"PUESTO"           VARCHAR2(50 BYTE)        NOT NULL ENABLE 
+    ,"DESCRIPCION"      VARCHAR2(250 BYTE)       NOT NULL ENABLE
+    ,"EDITADOPOR"       VARCHAR2(10 BYTE)        NOT NULL ENABLE
+    ,"HABILITADO"       NUMBER(1,0)              NOT NULL ENABLE
+    ,"FECHACREACION"    DATE                     NOT NULL ENABLE
+    ,PRIMARY KEY ("PUESTOTRABAJOID")
+  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  TABLESPACE "USERS"  ENABLE
+) SEGMENT CREATION DEFERRED 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  TABLESPACE "USERS" ;
+/****************************************************************************************************************************************************************
+Autor: José Andrés Alvarado Matamoros
+Requerimiento: AR-001
+Fecha Creación: 06/07/2024   (MM/dd/YYYY)
+Enunciado: Esta tabla se creo  para guardar la informacion del rol de los usaurios
+****************************************************************************************************************************************************************/  
+CREATE TABLE "HR"."ROL" 
+(	
+     "ROLID" NUMBER GENERATED ALWAYS AS IDENTITY MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  NOT NULL ENABLE
+	,"NOMBRE"            VARCHAR2(100 BYTE)     NOT NULL ENABLE
+	,"DESCRIPCION"       VARCHAR2(250 BYTE)     NOT NULL ENABLE
+	,"EDITADOPOR"        VARCHAR2(10 BYTE)      NOT NULL ENABLE
+	,"HABILITADO"        NUMBER(1,0)            NOT NULL ENABLE
+	,"FECHACREACION"     DATE                   NOT NULL ENABLE
+    ,PRIMARY KEY ("ROLID")
   USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
   TABLESPACE "USERS"  ENABLE
    ) SEGMENT CREATION DEFERRED 
   PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
  NOCOMPRESS LOGGING
   TABLESPACE "USERS" ;
+
+/****************************************************************************************************************************************************************
+Autor: José Andrés Alvarado Matamoros
+Requerimiento: AR-001
+Fecha Creación: 06/07/2024   (MM/dd/YYYY)
+Enunciado: Esta tabla se creo  para guardar la informacion del personal
+****************************************************************************************************************************************************************/  
+CREATE TABLE "HR"."PERSONAL" 
+(	
+     "CREDENCIALID"      VARCHAR2(20 BYTE)         NOT NULL ENABLE 
+	,"ROLID"             NUMBER                    NOT NULL ENABLE 
+	,"NOMBRE"            VARCHAR2(80 BYTE)         NOT NULL ENABLE 
+	,"PRIMERAPELLIDO"    VARCHAR2(80 BYTE)         NOT NULL ENABLE 
+	,"SEGUNDOAPELLIDO"   VARCHAR2(80 BYTE)         NOT NULL ENABLE 
+	,"FECHANACIMIENTO"   DATE                      NOT NULL ENABLE 
+	,"CODTRABAJADOR"     VARCHAR2(10 BYTE)         NOT NULL ENABLE 
+	,"FECHACONTRATACION" DATE                      NOT NULL ENABLE 
+	,"PUESTOTRABAJOID"   NUMBER                    NOT NULL ENABLE 
+	,"EDITADOPOR"        VARCHAR2(10 BYTE) 
+	,"HABILITADO"        NUMBER(1,0)               NOT NULL ENABLE 
+	,"FECHACREACION"     DATE                      NOT NULL ENABLE 
+	,CONSTRAINT "PERSONAL_PK" PRIMARY KEY ("CREDENCIALID")
+  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  TABLESPACE "USERS"  ENABLE, 
+	 CONSTRAINT "PERSONAL_ROL_FK" FOREIGN KEY ("ROLID")
+	  REFERENCES "HR"."ROL" ("ROLID") ENABLE
+) SEGMENT CREATION DEFERRED 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  TABLESPACE "USERS" ;
+
+/****************************************************************************************************************************************************************
+Autor: José Andrés Alvarado Matamoros
+Requerimiento: AR-001
+Fecha Creación: 06/07/2024   (MM/dd/YYYY)
+Enunciado: Esta tabla se creo  para guardar la informacion del cliente
+****************************************************************************************************************************************************************/  
+CREATE TABLE "HR"."CLIENTE" 
+(
+    "CREDENCIALID"             VARCHAR2(20 BYTE)     NOT NULL ENABLE 
+	,"ROLID"                   NUMBER                NOT NULL ENABLE
+	,"NOMBRE"                  VARCHAR2(80 BYTE)     NOT NULL ENABLE
+	,"PRIMERAPELLIDO"          VARCHAR2(80 BYTE)     NOT NULL ENABLE
+	,"SEGUNDOAPELLIDO"         VARCHAR2(80 BYTE)     NOT NULL ENABLE
+	,"FECHANACIMIENTO"         DATE                  NOT NULL ENABLE
+	,"EDITADOPOR"              VARCHAR2(10 BYTE)     NOT NULL ENABLE
+	,"HABILITADO" NUMBER(1,0) NOT NULL ENABLE
+	,"FECHACREACION" DATE NOT NULL ENABLE
+	, CONSTRAINT "CLIENTE_PK" PRIMARY KEY ("CREDENCIALID")
+  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  TABLESPACE "USERS"  ENABLE, 
+	 CONSTRAINT "CLIENTE_ROL_FK" FOREIGN KEY ("ROLID")
+	  REFERENCES "HR"."ROL" ("ROLID") ENABLE
+) SEGMENT CREATION DEFERRED 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  TABLESPACE "USERS" ;
+
+/****************************************************************************************************************************************************************
+Autor: José Andrés Alvarado Matamoros
+Requerimiento: AR-001
+Fecha Creación: 06/07/2024   (MM/dd/YYYY)
+Enunciado: Esta tabla se creo  para guardar la informacion de los diferentes tipos de categorias telefoncias.
+****************************************************************************************************************************************************************/  
+CREATE TABLE "HR"."CATEGORIATELEFONO" 
+(
+     "CATEGORIATELEFONOID" NUMBER GENERATED ALWAYS AS IDENTITY MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  NOT NULL ENABLE
+	,"TIPOTELEFONO" VARCHAR2(50 BYTE) NOT NULL ENABLE
+	,"DESCRIPCION" VARCHAR2(250 BYTE) NOT NULL ENABLE 
+	,"EDITADOPOR" VARCHAR2(10 BYTE) NOT NULL ENABLE
+	,"HABILITADO" NUMBER(1,0) NOT NULL ENABLE
+	,"FECHACREACION" DATE NOT NULL ENABLE
+	,PRIMARY KEY ("CATEGORIATELEFONOID")
+  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  TABLESPACE "USERS"  ENABLE
+) SEGMENT CREATION DEFERRED 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  TABLESPACE "USERS" ;
+  
+/****************************************************************************************************************************************************************
+Autor: José Andrés Alvarado Matamoros
+Requerimiento: AR-001
+Fecha Creación: 06/07/2024   (MM/dd/YYYY)
+Enunciado: Esta tabla se creo  para guardar la informacion de los telefonos por usaurio.
+****************************************************************************************************************************************************************/  
+CREATE TABLE "HR"."TELEFONOPORUSUARIO" 
+(	
+     "NUMEROTELEFONO" VARCHAR2(10 BYTE) NOT NULL ENABLE 
+	,"CREDENCIALID" VARCHAR2(20 BYTE) NOT NULL ENABLE
+	,"CATEGORIATELEFONOID" NUMBER NOT NULL ENABLE 
+	,"NUMEROEXTENSION" NUMBER(8,0) 
+	,"DESCRIPCION" VARCHAR2(250 BYTE)
+	,"EDITADOPOR" VARCHAR2(10 BYTE) NOT NULL ENABLE
+	,"HABILITADO" NUMBER(1,0) NOT NULL ENABLE
+	,"FECHACREACION" DATE NOT NULL ENABLE
+	 CONSTRAINT "TELEFONOPORUSUARIO_PK" PRIMARY KEY ("NUMEROTELEFONO", "CREDENCIALID")
+  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  TABLESPACE "USERS"  ENABLE, 
+	 CONSTRAINT "TELEFONOPORUSUARIO_CATEGORIATELEFONO_FK" FOREIGN KEY ("CATEGORIATELEFONOID")
+	  REFERENCES "HR"."CATEGORIATELEFONO" ("CATEGORIATELEFONOID") ENABLE, 
+	 CONSTRAINT "TELEFONOPORUSUARIO_PERSONAL_FK" FOREIGN KEY ("CREDENCIALID")
+	  REFERENCES "HR"."PERSONAL" ("CREDENCIALID") ENABLE, 
+	 CONSTRAINT "TELEFONOPORUSUARIO_CLIENTE_FK" FOREIGN KEY ("CREDENCIALID")
+	  REFERENCES "HR"."CLIENTE" ("CREDENCIALID") ENABLE
+) SEGMENT CREATION DEFERRED 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  TABLESPACE "USERS" ;
+
+ 
+
+
